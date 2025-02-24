1543c0ef820ab1291ed462e5df50ed173945a458
Yuki Nagino <y-nagino@vost.co.jp>
2025-02-24 13:40:58 +0900

    Initial commit

---
+++ ica.m
@@ -0,0 +1,20 @@
+% specify the eeglab folder path 
+% this is git submodule
+eeglabpath='./eeglab';
+addpath(eeglabpath);
+eeglab
+
+optionfile = getenv('OPTION_FILE_NAME');
+if isempty(optionfile)
+    optionfile = 'options.mat';
+end
+load(optionfile);
+
+datafile = getenv('DATA_FILE_NAME');
+if isempty(datafile)
+    datafile = 'test.mat';
+end
+load(datafile);
+
+[EEG.icaweights, EEG.icasphere] = runica(tmpdata, 'lrate', 0.001,  g.options{:});
+save('result.mat', 'EEG', '-v7.3');
